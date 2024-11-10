564560fa419b1f28008fb84fef91209507b20a1a
Kasun Lakshitha <kasun2014@outlook.com>
2024-11-11 00:21:25 +0530

    side bar setup finished

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <aside className="flex-none w-72 bg-white border-r">
       <div className="flex h-24 items-center justify-center">
@@ -17,18 +21,38 @@ export default function Sidebar() {
           </div>
         </Link>
       </div>
-      <menu className="px-8 py-10">
+      <menu className=" py-6">
         <li>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link
+            href="/dashboard"
+            className={`block px-8 py-4 ${pathname === "/dashboard" ? "bg-kBlue font-bold" : ""}`}
+          >
+            Dashboard
+          </Link>
         </li>
         <li>
-          <Link href="/dashboard">Expenses</Link>
+          <Link
+            href="/dashboard/expenses"
+            className={`block px-8 py-4 ${pathname === "/dashboard/expenses" ? "bg-kBlue font-bold" : ""}`}
+          >
+            Expenses
+          </Link>
         </li>
         <li>
-          <Link href="/dashboard">Reports</Link>
+          <Link
+            href="/dashboard/reports"
+            className={`block px-8 py-4 ${pathname === "/dashboard/reports" ? "bg-kBlue font-bold" : ""}`}
+          >
+            Reports
+          </Link>
         </li>
         <li>
-          <Link href="/dashboard">Settings</Link>
+          <Link
+            href="/dashboard/settings"
+            className={`block px-8 py-4 ${pathname === "/dashboard/settings" ? "bg-kBlue font-bold" : ""}`}
+          >
+            Settings
+          </Link>
         </li>
       </menu>
     </aside>
